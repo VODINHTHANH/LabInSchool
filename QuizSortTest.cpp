8716a7caf5edec4b394a46c91d1cd72ddd6619df
VODINHTHANH <58946497+VODINHTHANH@users.noreply.github.com>
2020-10-31 19:30:47 +0700

    Update QuizSortTest.cpp

--- QuizSortTest.cpp
+++ QuizSortTest.cpp
@@ -1,69 +1,57 @@
+#include <sstream>
 #include <iostream>
-
+#include <type_traits>
 using namespace std;
-
-
- 
- template <class T>
-void swap(T* &a, T* &b)
-{
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
- 
- template <class T>
-T* Partition (T* start, T* end)
-{
-    int pivot = *start;    // pivot
+template <class T>
+class Sorting {
+private:
+    static T* Partition(T* start, T* end) ;
+public:
+    static void QuickSort(T* start, T* end) ;
+};
+template <class T>
+T* Sorting<T>::Partition(T* start, T* end) {
+    // TODO: return the pointer which points to the pivot after rearrange the array.
+    T pivot = *start;  
     T* left = start+1;
     T* right = end-1;
     while(true){
-        while(left <= right && *left < pivot) left++;
-        while(right >= left && *right > pivot) right--;
+        while(left <= right && (*left) < pivot) left++;
+        while(right >= left && (*right) > pivot) right--;
         if (left >= right) break;
-        swap(left, right);
+        T t = *left;
+    *left = *right;
+    *right = t;
         left++;
         right--;
     }
-    swap(start, right);
+    T t = *start;
+    *start = *right;
+    *right = t;
     return right;
 }
+ 
 template <class T>
-/* Hàm thực hiện giải thuật quick sort */
-void QuickSort(T* start, T* end)
-{ static bool x = true;
+void Sorting<T>::QuickSort(T* start, T* end) {
+    // TODO
+    // In this question, you must print out the index of pivot in subarray after everytime calling method Partition.
+     static bool x = true;
     if (start < end)
     {
-        /* pi là chỉ số nơi phần tử này đã đứng đúng vị trí
-         và là phần tử chia mảng làm 2 mảng con trái & phải */
         T* pi  = Partition(start, end);
-       if(x==false) cout << "x";
+       if(x == false) cout << " ";
         cout << pi - start;
         x = false;
-        
-        
-        // Gọi đệ quy sắp xếp 2 mảng con trái và phải
-        QuickSort(start, pi - 1);
+        QuickSort(start, pi);
         QuickSort(pi + 1, end);
     }
 }
- 
-/* Hàm xuất mảng */
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
- 
- 
 int main()
 {
-    int array[] = { 3, 5, 7, 10 ,12, 14, 15, 13, 1, 2, 9, 6, 4, 8, 11, 16, 17, 18, 20, 19 };
-    QuickSort(&array[0], &array[20]);
-     int n = sizeof(array)/sizeof(array[0]);
-    printArray(array, n);
-    return 0;
+int array[] = { 3, 5, 7, 10 ,12, 14, 15, 13, 1, 2, 9, 6, 4, 8, 11, 16, 17, 18, 20, 19 };
+cout << "Index of pivots: ";
+Sorting<int>::QuickSort(&array[0], &array[20]);
+cout << "\n";
+cout << "Array after sorting: ";
+for (int i : array) cout << i << " ";
 }
