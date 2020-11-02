da236ae92b8ce146f8470df4de083a7be534de95
VODINHTHANH <58946497+VODINHTHANH@users.noreply.github.com>
2020-11-02 20:13:37 +0700

    Update HybridQuickSort.cpp
    check

--- HybridQuickSort.cpp
+++ HybridQuickSort.cpp
@@ -8,6 +8,7 @@ private:
     static T *Partition(T *start, T *end);
 
 public:
+static void QuickSort(T* start, T* end,int min_size) ;
     static void printArray(T *start, T *end)
     {
         int size = end - start;
@@ -24,73 +25,73 @@ public:
 template <class T>
 T *Sorting<T>::Partition(T *start, T *end)
 {
+     // TODO: return the pointer which points to the pivot after rearrange the array.
+    T pivot = *start;  
+    T* left = start+1;
+    T* right = end-1;
+    while(true){
+        while(left <= right && (*left) < pivot) left++;
+        while(right >= left && (*right) >= pivot) right--;
+        if (left >= right) break;
+        T t = *left;
+    *left = *right;
+    *right = t;
+        left++;
+        right--;
+    }
+    T t = *start;
+    *start = *right;
+    *right = t;
+    return right;
 }
-
+template <class T>
+void Sorting<T>::QuickSort(T* start, T* end, int min_size) {
+    if(end-start< min_size && end-start > 0)
+    {
+        cout << "Insertion sort: ";
+        printArray(start,end);
+        insertionSort(start,end);
+        
+    }
+   if(end-start>=min_size)
+    {   cout << "Quick sort: ";
+        printArray(start,end);
+        T* pi  = Partition(start, end);
+        QuickSort(start, pi, min_size);
+        QuickSort(pi + 1, end, min_size);
+    }
+}
 template <class T>
 void Sorting<T>::insertionSort(T *start, T *end)
 {
      for(T* i=start+1;i<end;i++) 
     { 
-      T* val = i ; 
+      T val = *i ; 
       T* j = i ; 
-    while (j>start && *(j-1)>*val) 
+    while (j>start && *(j-1)>val) 
       { 
         *j= *(j-1) ; 
       j-= 1; 
     } 
-    *j= *val ; 
+    *j= val ; 
     }  
 }
+
 template <class T>
 void Sorting<T>::hybridQuickSort(T *start, T *end, int min_size)
 {
-     while (start < end-1)  
-    { 
-  
-    // If the size of the array is less  
-    // than threshold apply insertion sort  
-    // and stop recursion  
-  
-    if (end-start < min_size) 
-      { 
-        insertionSort(start,end); 
-      break; 
+   if (start < end)
+    {
+        T* pi  = Partition(start, end);
+        QuickSort(start, pi);
+        QuickSort(pi + 1, end);
     } 
-  
-    else 
-        
-        { 
-          T* pivot = Partition(start,end) ; 
-  
-      // Optimised quicksort which works on  
-      // the smaller arrays first  
-  
-      // If the left side of the pivot  
-      // is less than right, sort left part  
-      // and move to the right part of the array  
-  
-      if (pivot-start<end-1-pivot) 
-        { 
-          hybridQuickSort(start,end);  
-        start = pivot + 1; 
-      } 
-      else
-        { 
-  
-        // If the right side of pivot is less  
-        // than left, sort right side and  
-        // move to the left side  
-            
-        hybridQuickSort(pivot + 1, end); 
-        end = pivot; 
-        } 
-  
-     } 
-  
-   } 
 }
 int main()
 {
-    int array[] = {1, 2, 6, 4, 7, 8, 5, 3};
-    Sorting<int>::insertionSort(&array[0],&array[8]);
+int array[] = {2, 6, 4, 12, 23, 1, 0, -12};
+int min_size = 4;
+Sorting<int>::QuickSort(&array[0], &array[8], min_size);
+cout << endl;
+for (int i : array) cout << i << " ";
 }
